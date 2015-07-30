ae75e4d6ca2fc4beb0cabba066ec55616d87539b
Andreas Hilboll <andreas@hilboll.de>
2015-07-30 09:19:27 +0200

    ENH: mkdir and lsdir functions

--- seafile.el
+++ seafile.el
@@ -125,18 +125,14 @@
 ; ----------------------------------------------------------------------
 ; Check if directory for today exists
 ; ----------------------------------------------------------------------
-; curl -H "Authorization: Token f2210dacd9c6ccb8133606d94ff8e61d9b477fd" -H 'Accept: application/json; indent=4' https://cloud.seafile.com/api2/repos/99b758e6-91ab-4265-b705-925367374cf0/dir/?p=/
-; (format-time-string "%Y-%m-%d") gives the current date
-; Response is vector of plist; "type" == "dir", "name" == (format-time-string ...), "id" = ...
-(defun seafile/check-for-directory-in-library (target library_id host user pass)
-  "Check if TARGET exists in LIBRARY_ID
-Currently checks only in root of library"
+(defun seafile/lsdir (path library_id host user pass)
+  "List entries of directory PATH in LIBRARY_ID"
   (progn
-    (setq found_directory nil)
+    (setq dirlist nil)
     (let ((auth_header (concat "Token " (seafile/get-auth-token host user pass)))
 	  (json-object-type 'plist))
       (request
-       (concat host_url "/api2/repos/" library_id "/dir/?p=/")
+       (concat host_url "/api2/repos/" library_id "/dir/?p=" path)  ; TODO check leading slash
        :type "GET"
        :headers `(("Authorization" . ,auth_header)
 		  ("Accept" . "application/json; indent=4"))
@@ -145,11 +141,7 @@ Currently checks only in root of library"
        :success (function*
 		 (lambda (&key data &allow-other-keys)
 		   (when data
-		     (setq days_list (mapcar 'identity (json-read-from-string data)))
-		     (dolist (day days_list)
-		       (when (and (equal target (plist-get day :name))
-				  (equal "dir" (plist-get day :type)))
-			 (setq found_directory target))))))
+		     (setq dirlist (mapcar 'identity (json-read-from-string data))))))
        :error
        (function* (lambda (&key error-thrown &allow-other-keys&rest _)
                     (message "Got error: %S" error-thrown)))
@@ -167,33 +159,95 @@ Currently checks only in root of library"
                       (441 . (lambda (&rest _) (message "Got 441 (REPO_PASSWD_MAGIC_REQUIRED).")))
                       (500 . (lambda (&rest _) (message "Got 500 (INTERNAL_SERVER_ERROR).")))
                       (520 . (lambda (&rest _) (message "Got 520 (OPERATION_FAILED)."))))))
-       found_directory))
+       dirlist))
 
-(setq my-today-directory-path (seafile/check-for-directory-in-library (format-time-string "%Y-%m-%d") my-attachments-library-id host_url host_user host_pass))
+(defun seafile/check-if-directory-exists (target library_id host user pass)
+  ; TODO: allow for directories not in '/'
+  (progn
+    (setq found_directory nil)
+    (setq days_list (seafile/lsdir "/" library_id host user pass))
+    (dolist (day days_list)
+      (when (and (equal target (plist-get day :name))
+		 (equal "dir" (plist-get day :type)))
+	(setq found_directory target)))
+    found_directory))
+
+(setq my-today-directory-path (seafile/check-if-directory-exists (format-time-string "%Y-%m-%d") my-attachments-library-id host_url host_user host_pass))
+
+(defun filelink/create-daydir (folder library_id host user pass)
+  ()
+  )
 
 
 ; ----------------------------------------------------------------------
 ; Create directory for today
 ; ----------------------------------------------------------------------
-; curl -d  "operation=mkdir" -v  -H 'Authorization: Token f2210dacd9c6ccb8133606d94ff8e61d99b477fd' -H 'Accept: application/json; charset=utf-8; indent=4' https://cloud.seafile.com/api2/repos/dae8cecc-2359-4d33-aa42-01b7846c4b32/dir/?p=/foo
-; /foo is the to-be-created directory
+(defun seafile/mkdir (target library_id host user pass)
+  "Creates directory TARGET in LIBRARY_ID"
+  (progn
+    (setq found_directory nil)
+    (let ((auth_header (concat "Token " (seafile/get-auth-token host user pass)))
+	  (json-object-type 'plist))
+      (request
+       (concat host_url "/api2/repos/" library_id "/dir/?p=" target)  ; TODO check for leading slash
+       :type "POST"
+       :headers `(("Authorization" . ,auth_header)
+		  ("Accept" . "application/json; indent=4"))
+       :data `(("operation" . "mkdir"))
+       :parser 'buffer-string
+       :sync t
+       :success (function*
+		 (lambda (&key response &allow-other-keys)
+		   (setq created_directory (request-response-header response "Location"))))
+       :error
+       (function* (lambda (&key error-thrown &allow-other-keys&rest _)
+                    (message "Got error: %S" error-thrown)))
+       :complete (lambda (&rest _) (message "Finished (get mkdir)!"))
+       :status-code '((200 . (lambda (&rest _) (message "Got 200 (OK).")))
+                      (201 . (lambda (&rest _) (message "Got 201 (CREATED).")))
+                      (202 . (lambda (&rest _) (message "Got 202 (ACCEPTED).")))
+                      (301 . (lambda (&rest _) (message "Got 301 (MOVED_PERMANENTLY).")))
+                      (400 . (lambda (&rest _) (message "Got 400 (BAD_request).")))
+                      (403 . (lambda (&rest _) (message "Got 403 (FORBIDDEN).")))
+                      (404 . (lambda (&rest _) (message "Got 404 (NOT_FOUND).")))
+                      (409 . (lambda (&rest _) (message "Got 409 (CONFLICT).")))
+                      (429 . (lambda (&rest _) (message "Got 429 (TOO_MANY_REQUESTS).")))
+                      (440 . (lambda (&rest _) (message "Got 440 (REPO_PASSWD_REQUIRED).")))
+                      (441 . (lambda (&rest _) (message "Got 441 (REPO_PASSWD_MAGIC_REQUIRED).")))
+                      (500 . (lambda (&rest _) (message "Got 500 (INTERNAL_SERVER_ERROR).")))
+                      (520 . (lambda (&rest _) (message "Got 520 (OPERATION_FAILED)."))))))
+       created_directory))
 
+(unless my-today-directory-path
+  (setq my-today-directory-path (seafile/mkdir (format-time-string "/%Y-%m-%d") my-attachments-library-id host_url host_user host_pass)))
 
 ; ----------------------------------------------------------------------
 ; Check if file exists in directory for today
 ; ----------------------------------------------------------------------
-; curl -H "Authorization: Token f2210dacd9c6ccb8133606d94ff8e61d9b477fd" -H 'Accept: application/json; indent=4' https://cloud.seafile.com/api2/repos/99b758e6-91ab-4265-b705-925367374cf0/dir/?p=/foo
+(defun seafile/check-if-file-exists (name directory library_id host user pass)
+  (progn
+    (setq found_file nil)
+    (setq dir_list (seafile/lsdir directory library_id host user pass))
+    (dolist (file dir_list)
+      (when (and (equal name (plist-get file :name))
+		 (equal "file" (plist-get file :type)))
+	(setq found_file name)))
+    found_file))
+
+(seafile/check-if-file-exists "asdf" (format-time-string "/%Y-%m-%d") my-attachments-library-id host_url host_user host_pass)
 
 ; ----------------------------------------------------------------------
 ; Get upload link
 ; ----------------------------------------------------------------------
 ; curl -H "Authorization: Token f2210dacd9c6ccb8133606d94ff8e61d99b477fd" https://cloud.seafile.com/api2/repos/99b758e6-91ab-4265-b705-925367374cf0/upload-link/
+; "http://cloud.seafile.com:8082/upload-api/ef881b22"
 
 
 ; ----------------------------------------------------------------------
 ; Upload file
 ; ----------------------------------------------------------------------
 ; curl -H "Authorization: Token f2210dacd9c6ccb8133606d94ff8e61d99b477fd" -F file=@test.txt -F filename=test.txt -F parent_dir=/ http://cloud.seafile.com:8082/upload-api/ef881b22
+; "adc83b19e793491b1c6ea0fd8b46cd9f32e592fc"
 
 
 ; ----------------------------------------------------------------------
